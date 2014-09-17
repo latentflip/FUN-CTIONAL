cd49f485513677762313afea2a03f74d8df8b992
Philip Roberts <phil@andyet.net>
2014-09-17 21:46:53 +0100

    Up and down

--- skipjack/project.clj
+++ skipjack/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
+                 [http-kit "2.1.16"]]
   :main ^:skip-aot skipjack.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- skipjack/src/skipjack/core.clj
+++ skipjack/src/skipjack/core.clj
@@ -1,7 +1,42 @@
 (ns skipjack.core
-  (:gen-class))
+  (:gen-class)
+  (:use org.httpkit.server))
+
+
+(def state (atom 0))
+(defn incr-value []
+  (swap! state inc))
+
+(defn decr-value []
+  (swap! state dec))
+
+(defn ok [body]
+  { :status 200
+    :headers {"Content-Type" "text/plain"}
+    :body (str body)})
+
+(defn increment-handler [channel]
+  (send! channel (ok (incr-value))))
+
+(defn decrement-handler [channel]
+  (send! channel (ok (decr-value))))
+
+
+(defn async-handler [ring-request]
+  (println ring-request)
+  (println (:uri ring-request))
+  ;; unified API for WebSocket and HTTP long polling/streaming
+  (with-channel ring-request channel    ; get the channel
+    (if (websocket? channel)            ; if you want to distinguish them
+      (on-receive channel (fn [data]     ; two way communication
+                            (send! channel data)))
+      (condp = (:uri ring-request)
+        "/up" (increment-handler channel)
+        "/down" (decrement-handler channel)
+        (println "No handler")
+        ))))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (run-server async-handler {:port 8080}))
